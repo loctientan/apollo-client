d802df7e024aaf32c6e0baa0b39f7d5d6bcf565f
phuong.nguyen <phuong.nguyen@kyanon.digital>
2021-08-30 16:58:39 +0700

    fix base src

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
   
-<<<<<<< HEAD
 import { BrowserRouter as Route, Switch, Redirect } from "react-router-dom";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
-=======
-import { BrowserRouter as Route, Switch, Redirect, Router } from "react-router-dom";
-import Footer from "./layout/Footer/index";
-import Header from "./layout/Header/header";
->>>>>>> dc89ca3ada5916f5236603ecd9974df74561ad32
 import Home from "./pages/Home/home";
 import "./styles/app.scss";
 
